f928acd54f9ec7e2cf9a977b705ece6c969ae5df
Travis Fantina <tfantina@gmail.com>
2024-12-01 22:24:19 -0500

    Day 1 - part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,53 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    /* this first example is copied from [matthsp](https://github.com/matthsp/aoc-2024-rust/tree/main) 
+     I have zero Rust experience, aside from reading the first 4 chapters of "The Book" this was a little flight check to make 
+     sure I had AOC configured correctly */
+
+    let (mut left, mut right) = input
+        .lines()
+        .map(|l| {
+            let mut split = l.split_whitespace();
+            let left = split.next();
+            let right = split.next();
+
+            (
+                left.unwrap().parse::<u32>().unwrap(),
+                right.unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .collect::<(Vec<_>, Vec<_>)>();
+
+    left.sort();
+    right.sort();
+
+    let mut diff: u32 = 0;
+
+    for (i, l) in left.iter().enumerate() {
+        diff += l.abs_diff(right[i]);
+    }
+
+    Some(left.iter().zip(right).map(|(a, b)| a.abs_diff(b)).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (mut left, mut right) = input.lines().map
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
